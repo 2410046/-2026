44f6dc02527b3593c0650a55c8d214a59d11257f
masue <2410046@i-seifu.jp>
2026-06-22 16:06:58 +0900

    up

--- 3D_Base/Source/Reaction/CReactionApply/CReactionApply.cpp
+++ 3D_Base/Source/Reaction/CReactionApply/CReactionApply.cpp
@@ -1,13 +1,2 @@
 #include "stdafx.h"
 #include "CReactionApply.h"
-
-std::shared_ptr<CReaction> CReactionApply::CreateBoost(CReaction::MoveType tag)
-{
-	std::shared_ptr<CBoost> pCollider->Apply
-    return pCollider;
-}
-
-std::shared_ptr<CReaction> CReactionApply::CreateKnockBack(CReaction::MoveType tag)
-{
-	return std::shared_ptr<CReaction>();
-}
